a37fd500eea1089de1dd76049590c0e768b3a75b
van <rivanghibran26@gmail.com>
2026-01-21 13:03:30 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-import 'src/app.dart';
-import 'features/insightmind/data/local/screening_record.dart';
+import 'src/app.dart'; 
+import 'features/insightmind/data/local/screening_record.dart'; 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Inisialisasi Hive
+  // 1. Init Hive
   await Hive.initFlutter();
 
-  // Registrasi adapter untuk model ScreeningRecord
+  // 2. Register Adapter (Wajib)
   Hive.registerAdapter(ScreeningRecordAdapter());
+  
 
-  // Membuka box untuk menyimpan riwayat screening
-  await Hive.openBox<ScreeningRecord>('screening_records');
 
-  // Membuka box untuk menyimpan jawaban sederhana (list<int>)
-  await Hive.openBox('insightmind_answers');
-
-  // Menjalankan aplikasi dengan Riverpod
   runApp(const ProviderScope(child: InsightMindApp()));
 }
